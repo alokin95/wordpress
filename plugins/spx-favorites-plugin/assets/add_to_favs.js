de4fe5b29e38b7e668aaa1053ce6428e32374f88
nikola.mitrovic <nikola.mitrovic@soprex.com>
2019-07-19 13:09:28 +0200

    finished js

--- plugins/spx-favorites-plugin/assets/add_to_favs.js
+++ plugins/spx-favorites-plugin/assets/add_to_favs.js
@@ -1,12 +1,28 @@
 jQuery(document).ready(function($){
    $('.add-to-favs-button').on('click', function() {
 
+       let self = $(this);
+
+       let post_id = $(this).data('id');
        let data = {
-           'action': 'add_to_favs'
+           'action': 'add_to_favs',
+           'post_id': post_id
        };
 
        $.post(ajax_object.ajax_url, data, function(response) {
-         console.log(response);
+           console.log();
+         if (self.hasClass('btn-primary')) {
+             self.removeClass('btn-primary');
+             self.addClass('btn-info');
+             self['context']['innerHTML'] = 'Remove from favorites';
+         }
+         else
+         {
+             self.removeClass('btn-info');
+             self.addClass('btn-primary');
+             self['context']['innerHTML'] = 'Add to favorites';
+         }
+
        })
    });
 });
--- plugins/spx-favorites-plugin/includes/Pages/Favorite.php
+++ plugins/spx-favorites-plugin/includes/Pages/Favorite.php
@@ -9,7 +9,8 @@ class Favorite extends Controller {
 
 	public function register() {
 		add_action('init', array($this, 'add_favorite_button'));
-		add_action('wp_ajax_add_to_favs', array( $this, 'favorite' ) );
+		add_action('wp_ajax_add_to_favs', array( $this, 'add_to_favs' ) );
+//		add_action('wp_ajax_nopriv_add_to_favs', array( $this, 'favorite' ) );
 	}
 
 	public function add_favorite_button() {
@@ -19,10 +20,47 @@ class Favorite extends Controller {
 	}
 
 	public function render_button($content) {
-		return $content . "<button type=\"button\" class=\"btn btn-primary add-to-favs-button\">Add to favorites</button>";
+		global $post;
+		global $wpdb;
+		$user_id = get_current_user_id();
+
+		$check_if_already_voted = $wpdb->get_results("SELECT * FROM spx_user_favorite_posts WHERE user_id = $user_id AND post_id = $post->ID");
+
+		if ( $check_if_already_voted ) {
+			return $content . "<button class=\"add-to-favs-button\" data-id=\"$post->ID\">Remove from favorites</button>";
+		}
+
+		return $content . "<button class=\"add-to-favs-button\" data-id=\"$post->ID\">Add to favorites</button>";
 	}
 
-	public function favorite() {
-		echo 'sadasda';
+	public function add_to_favs() {
+		global $wpdb;
+
+		$user_id = get_current_user_id();
+		$post_id = $_POST['post_id'];
+
+		$check_if_already_voted = $wpdb->get_results("SELECT * FROM spx_user_favorite_posts WHERE user_id = $user_id AND post_id = $post_id");
+
+		$response = array();
+
+		if ( $check_if_already_voted ) {
+			$wpdb->delete( 'spx_user_favorite_posts', array(
+				'user_id' => $user_id,
+				'post_id' => $post_id
+			) );
+			$response['message'] = 'Post removed from favorites';
+			wp_send_json($response);
+		}
+
+		$wpdb->insert(
+			'spx_user_favorite_posts',
+			array(
+				'user_id' => $user_id,
+				'post_id' => $post_id
+			)
+		);
+
+		$response['message'] = "Post successfully added to favorites";
+		wp_send_json($response);
 	}
 }
